5e696f9ceb5f685128b08c2499ea7656454efa4a
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-06-25 18:14:45 +0530

    Update config1.php

--- ecomm_project/config1.php
+++ ecomm_project/config1.php
@@ -6,9 +6,9 @@
 */
 class Config
  {
-    const HOST     = '172.27.59.54',
-          USER     = 'ecomm',
-          PASSWORD = 'ecomm@123',
+    const HOST     = '172.31.16.149',
+          USER     = 'akah',
+          PASSWORD = 'Password_123',
 		
 		HELPER = "Helper.php";
 } 
